3c3a1c1e9924714ab9e8bf7c4bd96bccfd0d5741
deyvid-dev <deyvidscm@hotmail.com>
2022-11-14 16:42:42 -0300

    feat: requisito 03

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -29,6 +29,9 @@ def scrape_novidades(html_content):
 # Requisito 3
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url_next_page = selector.css(".next ::attr(href)").get()
+    return url_next_page
 
 
 # Requisito 4
