610db3c20528aec48f183b3a5e4878960fe98865
deyvid-dev <deyvidscm@hotmail.com>
2022-11-14 19:31:19 -0300

    feat: requisito 07

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 6
@@ -14,6 +15,13 @@ def search_by_title(title):
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    try:
+        iso_format = datetime.fromisoformat(date).strftime("%d/%m/%Y")
+        news_date = search_news({"timestamp": iso_format})
+        result = [(news["title"], news["url"]) for news in news_date]
+        return result
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
