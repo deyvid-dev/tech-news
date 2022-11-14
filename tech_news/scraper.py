ca47ea325735bced43814a487e9bdfb573a1056f
deyvid-dev <deyvidscm@hotmail.com>
2022-11-14 18:43:42 -0300

    feat: requisito 04

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -37,6 +37,28 @@ def scrape_next_page_link(html_content):
 # Requisito 4
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css("link[rel=canonical]::attr(href)").get()
+    title = selector.css("h1.entry-title::text").get().strip()
+    timestamp = selector.css("li.meta-date::text").get()
+    writer = selector.css("a.url::text").get()
+    comments_count = len(selector.css(".comment-body").getall())
+    summary = "".join(
+            selector.css(".entry-content > p:nth-of-type(1) *::text")
+                .getall()).strip()
+    tags = selector.css(".post-tags a::text").getall()
+    category = selector.css("span.label::text").get()
+    news = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": comments_count,
+        "summary": summary,
+        "tags": tags,
+        "category": category
+    }
+    return news
 
 
 # Requisito 5
