2d5339d285bb5f0f92e9bbd38a3e6eb9898ab4df
deyvid-dev <deyvidscm@hotmail.com>
2022-11-14 19:15:17 -0300

    feat: requisito 06

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,8 +1,14 @@
+from tech_news.database import search_news
 
 
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    news_title = search_news({"title": {"$regex": title, "$options": "i"}})
+
+    result = [(news["title"], news["url"]) for news in news_title]
+
+    return result
 
 
 # Requisito 7
